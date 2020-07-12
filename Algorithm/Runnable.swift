6970e038d4101642feea02a2514a13707cf6fdee
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-07-12 18:49:39 +0900

    init

---
+++ Algorithm/Runnable.swift
@@ -0,0 +1,13 @@
+//
+//  Runnable.swift
+//  Algorithm
+//
+//  Created by Soso on 2020/07/12.
+//  Copyright © 2020 Soso. All rights reserved.
+//
+
+import Foundation
+
+protocol Runnable {
+    func run() -> Bool
+}
---
+++ Algorithm/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by Soso on 2020/07/12.
+//  Copyright © 2020 Soso. All rights reserved.
+//
+
+import Foundation
+
+//print(class().run())
